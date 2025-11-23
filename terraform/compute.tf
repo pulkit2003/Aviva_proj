d523a2b2963a26a5a2caa39ac3173c28fb63affc
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-11-23 21:10:18 +0530

    Update compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -63,7 +63,7 @@ data "aws_ami" "ubuntu" {
 # EC2 instance which will later upload a txt file to S3
 resource "aws_instance" "app" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
