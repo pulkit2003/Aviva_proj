ab2114b54e472ee9f0ef27f6cc10a6b03315b047
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-11-24 01:37:39 +0530

    Update default project name in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,5 +7,5 @@ variable "region" {
 variable "project_name" {
   description = "Project name prefix for tagging and naming resources"
   type        = string
-  default     = "ec2-s3-demo"
+  default     = "ec2-s3-demo_v2"
 }
