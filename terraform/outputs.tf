5ef4f22c105e3d32b686da64b2a1496a805a3e6a
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-11-23 17:41:13 +0530

    Add outputs for VPC, subnet, EC2, and S3 bucket

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.storage.bucket
+}
