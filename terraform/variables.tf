940cf13319dfed447ef952db107ba57890c29a5b
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-11-23 17:39:24 +0530

    Add variables for region and project name in Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name prefix for tagging and naming resources"
+  type        = string
+  default     = "ec2-s3-demo"
+}
