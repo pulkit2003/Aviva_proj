332d89b6f645bc210746383b970462a18ed09a48
Pulkit Mathur <94034822+pulkit2003@users.noreply.github.com>
2025-11-24 02:49:59 +0530

    Add key_name to EC2 instance configuration

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -64,6 +64,7 @@ resource "aws_instance" "app" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
+  key_name                    = "aviav-proj-kp"
 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
